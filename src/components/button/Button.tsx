8181d7a625b5cd59cd3dc197ad817a2131ee9984
Sam Seabourn <sam.seabourn@stan.com.au>
2022-09-27 09:47:26 +1000

    chore(src): add current fosters window WIP

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,12 +3,23 @@ import './style.css'
 interface Props {
 	text: React.ReactNode
 	onClickFn: () => void
+	disabled?: boolean
 }
 
-const Button = ({ text, onClickFn }: Props) => (
-	<div onClick={onClickFn} className='button'>
-		<span>{text}</span>
-	</div>
-)
+const Button = ({ text, onClickFn, disabled = false }: Props) => {
+	const handleClick = () => {
+		if (disabled) return
+		onClickFn()
+	}
+
+	return (
+		<div
+			onClick={handleClick}
+			className={`button ${disabled ? 'disabled' : ''}`}
+		>
+			<span>{text}</span>
+		</div>
+	)
+}
 
 export default Button
--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -13,51 +13,63 @@ const Catalog = ({ id, isActive, setActive, closeWindow }: ScoreProps) => {
 	const [fosterKitties, setFosterKitties] = useState([])
 	const [currentIndex, setCurrentIndex] = useState(0)
 
+	//TODO: add to preload image script
 	useEffect(() => {
 		fetch(
 			'https://raw.githubusercontent.com/SamSeabourn/CatJSON/main/cats.json'
 		)
 			.then(res => res.json())
 			.then(data => {
-				console.log()
 				setFosterKitties(data.currentKittens)
 			})
 	}, [])
 
+	const pageDown = () => {
+		if (currentIndex < 1) return
+		setCurrentIndex(currentIndex - 1)
+	}
+
+	const pageUp = () => {
+		if (currentIndex >= fosterKitties.length - 1) return
+		setCurrentIndex(currentIndex + 1)
+	}
+
 	return (
 		<OSWindow
 			id={id}
-			title='SCORE'
+			title='CURRENT FOSTER KITTENS'
 			size='medium'
 			startingShift={0}
 			setActive={setActive}
 			isActive={isActive}
 			closeWindow={closeWindow}
 		>
-			<div>
+			<div className='buttons'>
+				<Button
+					text='< Prev'
+					onClickFn={pageDown}
+					disabled={currentIndex < 1}
+				/>
+				<Button
+					text='Next >'
+					onClickFn={pageUp}
+					disabled={currentIndex >= fosterKitties.length - 1}
+				/>
+			</div>
+			<div className='wrapper'>
 				{fosterKitties?.map((fk: any, i) => {
 					if (currentIndex === i) {
 						return (
 							<>
-								<div>
+								<div
+									className='profile-pic'
+									style={{
+										backgroundImage: `url("${fk.imageUrl}")`,
+									}}
+								></div>
+								<div className='text--wrapper'>
 									<h2>{fk.name}</h2>
-									<div
-										className='profile-pic'
-										style={{
-											backgroundImage: `url("${fk.imageUrl}")`,
-										}}
-									></div>
 									<p>{fk.description}</p>
-								</div>
-								<div className='buttons'>
-									<Button
-										text='Prev'
-										onClickFn={console.log}
-									/>
-									<Button
-										text='Next'
-										onClickFn={console.log}
-									/>
 								</div>
 							</>
 						)
